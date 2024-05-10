ccd66f02d89ec31f13d399dec4d161a51ab61391
prince <nguhprince1@gmail.com>
2024-05-10 06:00:17 +0100

    Done with all the tasks in Practical Exercise, not tested

--- practical_exercises/practical_exercise_1.py
+++ practical_exercises/practical_exercise_1.py
@@ -1,10 +1,14 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, email=None, password=None):
         self._name = name
         self._age = age
 
+        # Task 6
+        self._email = email
+        self.__password = password
+
     def __str__(self):
-        return f"{self.name} {self.age} years"
+        return f"{self._name} {self._age} years"
     
     @property
     def age(self):
@@ -19,7 +23,27 @@ class Person:
                 self._age = new_age
         else:
             raise ValueError("Age has to be an integer")
+        
+    # Task 6
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, new_email):
+        # put email validation code here (TIP: regular expressions)
+        self.email = new_email
 
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, new_password):
+        if len(new_password) < 8:
+            raise ValueError("Password needs to be at least 8 characters")
+
+        self.__password = new_password
 
 
 class Employee(Person):
@@ -36,4 +60,32 @@ class Employee(Person):
     def employee_id(self, new_id):
         if isinstance(new_id, int):
             self.__employee_id = new_id
+
+    def __str__(self):
+        return f"Employee ID: {self.__employee_id}; {super().__str__()}"
+    
+    @classmethod
+    def from_string(cls, string):
+        parts = string.split(",")
+
+        if len(parts) < 3:
+            raise ValueError("The string has to contain the name, age and employee_id")
         
+        parts[1] = int(parts[1])
+        parts[2] = int(parts[2])
+
+        return Employee(name=parts[0], age=parts[1], employee_id=parts[2])
+
+
+class Customer(Person):
+    def __init__(self, name, age, customer_id):
+        super().__init__(name, age)
+        self.__customer_id = customer_id
+    
+    def __str__(self):
+        return f"Cust #{self.__customer_id}; {super().__str__()}"
+    
+
+# Task 5
+def print_info(person):
+    print(person.__str__())
